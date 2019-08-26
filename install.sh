a7130fea62cbccc3aa9cfcb638cc729ce69ad45f
frankiezafe <frankie@frankiezafe.org>
2019-08-26 10:32:34 +0200

    4 cores compilation

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ cd godot/modules
 git clone git@gitlab.com:frankiezafe/gosc.git gosc
 # running the installation of gosc
 cd gosc
-python3 install.py
+python install.py
 # back to godot
 cd ../../
 # installing dependencies (debian based distro)
 sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
 # compilation
-scons platform=x11
+scons platform=x11 -j4
 # engine is recompiled with gsoc module
 
 # getting the lavatar project
