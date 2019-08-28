a421345a5ae978d9e6828a2dd535411cfd8e4427
frankiezafe <frankie@frankiezafe.org>
2019-08-28 19:03:26 +0200

    bash script to push on gitlab and github at once

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "pushing to git@gitlab.com:polymorphcool/lavatar.git (origin)"
+git push origin
+echo "pushing to git@github.com:numediart/OpenFace.git (github)"
+git push github
