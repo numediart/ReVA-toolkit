09e876e468aef0c44e23b3155f6c60c02af15467
frankiezafe <frankie@frankiezafe.org>
2019-09-17 13:48:15 +0200

    preparing animation debugging

--- godot/scripts/tmp/root.gd
+++ godot/scripts/tmp/root.gd
@@ -4,10 +4,13 @@ const ReVA = preload( "res://scripts/reva/ReVA.gd" )
 var animation = null
 
 func _ready():
-	var animation = ReVA.load_animation( "../json/anim/smile.json" )
-	print( animation.errors )
-	var calibration = ReVA.load_calibration( "../json/anim/smile.json" )
-	print( calibration.errors )
+	
+	animation = ReVA.load_animation( "../json/anim/smile.json" )
+	if animation.success:
+		pass
+
+#	var calibration = ReVA.load_calibration( "../json/anim/smile.json" )
+#	print( calibration.errors )
 
 func _process(delta):
 	pass
