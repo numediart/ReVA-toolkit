4b5551d675e620fe84ffa50c9febe6de5d79426f
frankiezafe <frankie@frankiezafe.org>
2019-10-07 17:55:43 +0200

    preparation of raycasting

--- godot/scripts/tmp/3d2dpicker.gd
+++ godot/scripts/tmp/3d2dpicker.gd
@@ -1,25 +1,33 @@
 extends Node2D
 
-var normal_icon = preload("res://textures/svg/icon_status_error.svg")
-var selected_icon = preload("res://textures/svg/icon_play.svg")
-
+const color_normal = Color( 1,1,1 )
+const color_select = Color( 1,0,0 )
 onready var cam = get_node("../cam_pivot/cam")
 var pickables = null
-var btn_offset = Vector2(-8,-8)
+
+func prepare( root ):
+	while $pickers.get_child_count() > 0:
+		$pickers.remove_child( $pickers.get_child(0) )
+	pickables = []
+	for c in root.get_children():
+		if c is MeshInstance:
+			var pos = cam.unproject_position( c.global_transform.origin )
+			var btn = $tmpl.duplicate()
+			btn.cam = cam
+			btn.obj = c
+			$pickers.add_child(btn)
+			btn.connect( "picker3d_button_pressed", self, "on_pressed" )
 
 func _ready():
-	pickables = []
-	var r = get_node( "../" )
-	for c in r.get_children():
-		if c is MeshInstance:
-			pickables.append( {'o3d':c,'button':null} )
-	for p in pickables:
-		var pos = cam.unproject_position( p.o3d.global_transform.origin )
-		p.button = $tmpl.duplicate()
-		p.button.rect_position = pos + btn_offset
-		$pickers.add_child(p.button)
+	prepare( get_node( "../" ) )
 
 func _process(delta):
-	for p in pickables:
-		var pos = cam.unproject_position( p.o3d.global_transform.origin )
-		p.button.rect_position = pos + btn_offset
+	pass
+
+func on_pressed( btn ):
+	if btn.selected:
+		btn.obj.material_override = btn.obj.material_override.duplicate()
+		btn.obj.material_override.albedo_color = color_select
+	else:
+		btn.obj.material_override = btn.obj.material_override.duplicate()
+		btn.obj.material_override.albedo_color = color_normal
